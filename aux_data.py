ce1cad5bc08cfcb3cf0b429daa42af78d70c88c6
onrona_gitlab <158049091+onrona@users.noreply.github.com>
2025-11-21 18:12:07 +0100

    new experimental branch

---
+++ aux_data.py
@@ -0,0 +1,44 @@
+aux_dict_L1 = {
+    'ATL_CSC_1B': ['AC', 'BA'],	
+    'ATL_DCC_1B': ['AC', 'AD', 'AE', 'AF', 'BA', 'BC'],
+    'ATL_FSC_1B': ['AC', 'AD', 'AE', 'BA'],	
+    'ATL_NOM_1B': ['AC', 'AD', 'AE', 'BA', 'BC'],
+    'AUX_JSG_1D': ['AC', 'BA', 'BC'],	
+    'AUX_MET_1D': ['AA'],
+    'BBR_LIN_1B': ['BC'],
+    'BBR_NOM_1B': ['AE', 'AF', 'BA', 'BB', 'CA', 'CB', 'CC', 'CD', 'DA'],
+    'BBR_SNG_1B': ['AC', 'AD', 'BA', 'BC'],
+    'CPR_NOM_1B': ['AE', 'AF', 'BA', 'BB', 'CA', 'CB', 'CC', 'CD', 'DA'],
+    'MSI_BBS_1B': ['BC'],
+    'MSI_NOM_1B': ['AC', 'AD', 'AE', 'AF', 'BA', 'BC'],
+    'MSI_RGR_1C': ['AC', 'AD', 'AE', 'AF', 'BA', 'BC']
+}
+
+
+aux_dict_L2 = {
+    'AC__TC__2B':['AB', 'AC', 'AD', 'BA', 'CA'],	
+    'ACM_CAP_2B':['AC', 'AD', 'BA', 'BC'],
+    'ACM_COM_2B':['AC', 'BA', 'BC'],	
+    'ACM_RT__2B':['AB', 'AC', 'BA', 'BC'],
+    'ALL_3D__2B':['AB', 'BA', 'BC'],	
+    'ALL_DF__2B':['AB', 'BA', 'BC'],
+    'AM__ACD_2B':['AB',' AC', 'BA', 'BB', 'BC'],	
+    'AM__CTH_2B':['AB',' AC', 'BA', 'BB', 'BC'],
+    'ATL_AER_2A':['AC', 'AD', 'AE', 'AF', 'AG', 'BA', 'BB', 'BC'],	
+    'ATL_ALD_2A':['AC', 'AD', 'AE', 'BA', 'BB', 'BC'],
+    'ATL_CTH_2A':['AC', 'AD', 'AE', 'BA', 'BB', 'BC'],	
+    'ATL_EBD_2A':['AC', 'AD', 'AE', 'AF', 'AG', 'BA', 'BB', 'BC'],
+    'ATL_FM__2A':['AB',' AC', 'AD', 'BA', 'BB', 'BC'],	
+    'ATL_ICE_2A':['AC', 'AD', 'AE', 'AF', 'AG', 'BA', 'BB', 'BC'],
+    'ATL_TC__2A':['AC', 'AD', 'AE', 'AF', 'AG', 'BA', 'BB', 'BC'],	
+    'BM__RAD_2B':['AB', 'AC', 'BA', 'BC'],
+    'BMA_FLX_2B':['AA', 'AB', 'BA', 'BB', 'BC'],	
+    'CPR_APC_2A':['BA', 'BC'],
+    'CPR_CD__2A':['AB', 'AC', 'AD', 'BA', 'BC'],	
+    'CPR_CLD_2A':['AB', 'BA', 'BB', 'BC'],
+    'CPR_FMR_2A':['AB', 'AC', 'AD', 'BA', 'BC'],	
+    'CPR_TC__2A':['AB', 'AC', 'AD', 'BA', 'BC'],
+    'MSI_AOT_2A':['AB', 'AC', 'AD', 'BC'],	
+    'MSI_CM__2A':['AB', 'BA', 'BC'],
+    'MSI_COP_2A':['AB', 'BA', 'BC']
+}
